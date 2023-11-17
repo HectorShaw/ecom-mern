31e9e08725b142101294d0917739ce6cc1c80f19
HectorShaw <ogawde7@gmail.com>
2023-11-18 02:26:32 +0530

    database reconfig done

---
+++ server/src/errors.ts
@@ -0,0 +1,6 @@
+export enum UseErrors {
+    NO_USER_FOUND = "no-user-found",
+    WRONG_CREDENTIALS = "wrong-credentials",
+    USERNAME_ALREADY_EXISTS = "username-already-exists",
+    
+}
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,9 +1,23 @@
 import express from 'express'
-const router = express.Router();
+import { Router, Request, Response } from "express";
+import { UserModel } from '../models/user';
+import { mongo } from 'mongoose';
+import { UseErrors } from '../errors';
+
+const router = Router();
 
 
 // url="localhost:3001/register"
 
-router.post("/register");
+router.post("/register", async(req:Request, res:Response) => {
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username })
+    if (user){
+        return res.status(400).json({type: UseErrors.USERNAME_ALREADY_EXISTS})
+    }
+    const newUser = new UserModel({username, password})
+
+  });
 
 export {router as userRouter}
